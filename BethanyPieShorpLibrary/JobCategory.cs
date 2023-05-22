2a11deb9b369650740e4d82e7ee957aecbf8af44
bruno <bruno@SSD2>
2023-05-22 16:44:33 +0200

    Aggiungere i file di progetto.

---
+++ BethanyPieShorpLibrary/JobCategory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BethanyPieShorpLibrary
+{
+    public class JobCategory
+    {
+      public int JobCategoryId { get; set; }
+
+      public string JobCategoryName { get; set; }
+    }
+}
